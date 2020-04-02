f751118a5c79be98786a3d65dbbfe12744e94100
goliatone <burgosemi@gmail.com>
2020-04-02 15:12:31 -0700

    add module exports of components to index file

--- index.js
+++ index.js
@@ -8,3 +8,12 @@
  * a custom initializer.
  */
 module.exports.init = require('./lib/init');
+
+
+module.exports.CommandStatusManager = require('./lib/manager');
+
+module.exports.CommandStatus = require('./lib/status');
+
+module.exports.RedisStore = require('./lib/cache/redis');
+
+module.exports.MemoryStore = require('./lib/cache/memory');
